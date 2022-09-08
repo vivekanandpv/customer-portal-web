d6ccd8b5959cb2f00965be4dd4643bb248a435fb
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-08 12:16:17 +0530

    form created in component

--- src/app/add-customer/add-customer.component.ts
+++ src/app/add-customer/add-customer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { RestService } from '../_services/rest.service';
 
 @Component({
@@ -7,7 +13,27 @@ import { RestService } from '../_services/rest.service';
   styleUrls: ['./add-customer.component.scss'],
 })
 export class AddCustomerComponent implements OnInit {
-  constructor(private restService: RestService) {}
+  form: FormGroup;
+
+  constructor(private restService: RestService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstName: ['', [Validators.required, Validators.maxLength(50)]],
+      lastName: ['', [Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get firstName(): FormControl {
+    return this.form.controls['firstName'] as FormControl;
+  }
+
+  get lastName(): FormControl {
+    return this.form.controls['lastName'] as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.form.controls['email'] as FormControl;
+  }
 
   ngOnInit(): void {}
 }
