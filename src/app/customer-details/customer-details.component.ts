b87daee70b826cd2e5012ac59cc1657c0fcc8182
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-08 15:29:50 +0530

    done

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CustomerViewModel } from '../_models/customer.models';
 import { RestService } from '../_services/rest.service';
 
 @Component({
@@ -7,7 +10,15 @@ import { RestService } from '../_services/rest.service';
   styleUrls: ['./customer-details.component.scss'],
 })
 export class CustomerDetailsComponent implements OnInit {
-  constructor(private restService: RestService) {}
+  customerId: number;
+  customer$: Observable<CustomerViewModel>;
+
+  constructor(private restService: RestService, private ar: ActivatedRoute) {
+    this.customerId = +(this.ar.snapshot.paramMap.get('id') ?? 0);
+    this.customer$ = this.restService.read<CustomerViewModel>(
+      'http://localhost:8080/api/v1/customers/' + this.customerId
+    );
+  }
 
   ngOnInit(): void {}
 }
